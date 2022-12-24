389a54eefdd76faab471af05c376ff91dad563de
Tim Borowski <t.borowski@schenckprocess.com>
2022-12-24 12:18:42 +0100

    Day 20 and Day 21

---
+++ Day 20/Day 20/Program.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Day_20
+{
+    // 8556
+    // 8105
+    // 4439
+    // 21100 too high
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = File.ReadLines("input.txt").Select(long.Parse).Select(x => x * 811589153).ToArray();
+            
+            Dictionary<long, LinkedListNode<long>> nodeLookup = new();
+            LinkedList<long> linkedList = new();
+
+
+            LinkedListNode<long> val0 = null;
+
+            var i = 0;
+            foreach (var number in numbers)
+            {
+                var node = new LinkedListNode<long>(number);
+                nodeLookup.Add(i, node);
+                linkedList.AddLast(node);
+                i++;
+
+                if (number == 0)
+                {
+                    val0 = node;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", linkedList));
+            //Console.WriteLine(string.Join(", ", linkedList));
+
+            for (var z = 0; z < 10; z++)
+            {
+                for (var ii = 0; ii < linkedList.Count; ii++)
+                {
+                    Mix(ii, linkedList, nodeLookup);
+                    //Console.WriteLine(string.Join(", ", linkedList));
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", linkedList));
+            Console.WriteLine("finish");
+
+
+
+            var iterator = val0;
+            var sum = 0l;
+            for (long z = 0; z < 3; z++)
+            {
+                for (long t = 0; t < 1000; t++)
+                {
+                    if (iterator == linkedList.Last)
+                    {
+                        iterator = linkedList.First;
+                        continue;
+                    }
+                    iterator = iterator.Next;
+                }
+
+                sum += iterator.Value;
+                Console.WriteLine(iterator.Value);
+            }
+            Console.WriteLine(sum);
+
+        }
+
+        private static void Mix(long index, LinkedList<long> linkedList, Dictionary<long, LinkedListNode<long>> nodes)
+        {
+            var originalNode = nodes[index];
+
+            var numberToMix = originalNode.Value;
+            if (numberToMix > 0)
+            {
+                var iteratorNode = GetNextNode(linkedList, originalNode);
+                linkedList.Remove(originalNode);
+                var targetNode = GetNextNodeSlow(linkedList, numberToMix - 1, iteratorNode);
+                linkedList.AddAfter(targetNode, originalNode);
+            }
+            
+            if (numberToMix < 0)
+            {
+                var iteratorNode = GetPrevNode(linkedList, originalNode);
+                //Console.WriteLine($"prev node is {iteratorNode.Value}");
+                linkedList.Remove(originalNode);
+                var targetNode = GetPrevNodeSlow(linkedList, Math.Abs(numberToMix) - 1, iteratorNode);
+                linkedList.AddBefore(targetNode, originalNode);
+            }
+        }
+
+        private static LinkedListNode<long> GetNextNodeSlow(LinkedList<long> linkedList, long numberToMix, LinkedListNode<long> iteratorNode)
+        {
+            numberToMix %= linkedList.Count;
+            for (long i = 0; i < numberToMix; i++)
+            {
+                iteratorNode = GetNextNode(linkedList, iteratorNode);
+            }
+            return iteratorNode;
+        }
+
+        private static LinkedListNode<long> GetPrevNodeSlow(LinkedList<long> linkedList, long numberToMix, LinkedListNode<long> iteratorNode)
+        {
+            numberToMix %= linkedList.Count;
+            for (long i = 0; i < numberToMix; i++)
+            {
+                iteratorNode = GetPrevNode(linkedList, iteratorNode);
+                //Console.WriteLine($"iterator node is now {iteratorNode.Value}");
+            }
+            return iteratorNode;
+        }
+        
+        private static LinkedListNode<long> GetNextNode(LinkedList<long> linkedList, LinkedListNode<long> nodeIterator)
+        {
+            if (nodeIterator == linkedList.Last)
+            {
+                return linkedList.First;
+            }
+            return nodeIterator.Next;
+        }
+
+        private static LinkedListNode<long> GetPrevNode(LinkedList<long> linkedList, LinkedListNode<long> nodeIterator)
+        {
+            if (nodeIterator == linkedList.First)
+            {
+                return linkedList.Last;
+            }
+            return nodeIterator.Previous;
+        }
+    }
+
+    internal static class IndexOfExtension {
+
+        public static long IndexOf<T>(this LinkedList<T> linkedList, LinkedListNode<T> node)
+        {
+            var index = 0;
+            var iterator = linkedList.First;
+
+            while (iterator != node)
+            {
+                index++;
+                iterator = iterator.Next;
+            }
+
+            return index;
+        }
+
+        public static LinkedListNode<T> NodeAt<T>(this LinkedList<T> linkedList, long index)
+        {
+            var iterator = linkedList.First;
+
+            for (long i = 0; i < index; i++)
+            {
+                iterator = iterator.Next;
+            }
+
+            return iterator;
+        }
+
+    }
+}
---
+++ Day 21/Day 21/Program.cs
@@ -0,0 +1,433 @@
+﻿using System.Globalization;
+using System.Threading;
+
+namespace Day_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("input.txt");
+
+            var compiler = new Compiler();
+            var executionContext = compiler.Compile(lines);
+            executionContext.Init();
+            var root = executionContext.GetExpression("root");
+
+            executionContext.RefreshAllNumberValues();
+            Console.WriteLine($"Solution Part 1: {root.LastValue}");
+
+
+
+
+            var executionContextPart2 = compiler.Compile(lines);
+            
+            var newRoot = new SubtractExpression("gqjg", "rpjv");
+            executionContextPart2.AddExpression("root", newRoot);
+            executionContextPart2.Init();
+            executionContextPart2.RefreshAllNumberValues();
+
+            var human = (executionContextPart2.GetExpression("humn") as NumberExpression)!;
+            var currentPosition = 0d;
+
+            while (true)
+            {
+                var currentHeight = GetHeight(currentPosition, human, newRoot);
+                if (currentHeight == 0)
+                {
+                    Console.WriteLine($"Solution Part 2: {currentPosition}");
+                    return;
+                }
+
+                currentPosition = Newton(currentPosition, human, newRoot, currentHeight);
+                Console.WriteLine($"{currentPosition} .. {currentHeight}");
+            }
+        }
+
+        private static double Newton(double i, NumberExpression human, SubtractExpression root, double currentHeight)
+        {
+            var derivate = CalculateDerivate(i, human, root, currentHeight);
+            return Math.Floor( i - currentHeight / derivate);
+        }
+
+        private static double CalculateDerivate(double i, NumberExpression human, SubtractExpression root, double currentHeight)
+        {
+            var height1 = GetHeight(i + 1, human, root);
+            var derivate = height1 - currentHeight;
+            return derivate;
+        }
+
+        private static double GetHeight(double i, NumberExpression human, SubtractExpression root)
+        {
+            human.OverrideValue(i);
+            return root.LastValue!.Value;
+        }
+    }
+
+    internal class Compiler
+    {
+        public ExecutionContext Compile(string[] lines)
+        {
+            var context = new ExecutionContext();
+            foreach (var tokens in lines.Select(x => x.Split(" ")))
+            {
+                IExpression expression = tokens switch
+                {
+                    [_, var number] when int.TryParse(number, out var parsed) => new NumberExpression(parsed),
+                    [_, var var1, "+", var var2] => new AddExpression(var1, var2),
+                    [_, var var1, "-", var var2] => new SubtractExpression(var1, var2),
+                    [_, var var1, "*", var var2] => new MultiplyExpression(var1, var2),
+                    [_, var var1, "/", var var2] => new DivideExpression(var1, var2),
+                    _ => throw new InvalidOperationException("Syntax error")
+                };
+                context.AddExpression(tokens[0].Substring(0, tokens[0].Length -1), expression);
+            }
+            return context;
+        }
+        
+    }
+
+    internal class NumberExpression : IExpression
+    {
+        private readonly int _initValue;
+
+        public NumberExpression(int initValue)
+        {
+            _initValue = initValue;
+            LastValue = initValue;
+        }
+
+        public void PublishValue()
+        {
+            LastValue = _initValue;
+            Publish?.Invoke(this, LastValue.Value);
+        }
+
+        public double? LastValue { get; private set; }
+
+        public void Setup(ExecutionContext context)
+        {
+            // no dependencies
+        }
+
+        public event EventHandler<double>? Publish;
+
+        public void OverrideValue(double testNumber)
+        {
+            LastValue = testNumber;
+            Publish?.Invoke(this, LastValue.Value);
+        }
+    }
+    internal class AddExpression : IExpression
+    {
+        private readonly string _varName1;
+        private readonly string _varName2;
+
+        private double? _var1;
+        public double? Var1
+        {
+            get => _var1;
+            set
+            {
+                if (_var1 == value)
+                {
+                    return;
+                }
+                _var1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double? _var2;
+        public double? Var2
+        {
+            get => _var2;
+            set
+            {
+                if (_var2 == value)
+                {
+                    return;
+                }
+                _var2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged()
+        {
+            if (Var1 != null && Var2 != null)
+            {
+                LastValue = Var1.Value + Var2.Value;
+                Publish?.Invoke(this, LastValue.Value);
+            }
+        }
+
+        public AddExpression(string varName1, string varName2)
+        {
+            _varName1 = varName1;
+            _varName2 = varName2;
+        }
+
+        public double? LastValue { get; private set; }
+
+        public void Setup(ExecutionContext context)
+        {
+            context.GetExpression(_varName1).Publish += (_, i) =>
+            {
+                Var1 = i;
+            };
+            context.GetExpression(_varName2).Publish += (_, i) =>
+            {
+                Var2 = i;
+            };
+        }
+
+        public event EventHandler<double>? Publish;
+    }
+    internal class SubtractExpression : IExpression
+    {
+        private readonly string _varName1;
+        private readonly string _varName2;
+
+        private double? _var1;
+        public double? Var1
+        {
+            get => _var1;
+            set
+            {
+                if (_var1 == value)
+                {
+                    return;
+                }
+                _var1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double? _var2;
+        public double? Var2
+        {
+            get => _var2;
+            set
+            {
+                if (_var2 == value)
+                {
+                    return;
+                }
+                _var2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged()
+        {
+            if (Var1 != null && Var2 != null)
+            {
+                LastValue = Var1.Value - Var2.Value;
+                Publish?.Invoke(this, LastValue.Value);
+            }
+        }
+
+        public SubtractExpression(string varName1, string varName2)
+        {
+            _varName1 = varName1;
+            _varName2 = varName2;
+        }
+
+        public double? LastValue { get; private set; }
+
+        public void Setup(ExecutionContext context)
+        {
+            context.GetExpression(_varName1).Publish += (_, i) =>
+            {
+                Var1 = i;
+            };
+            context.GetExpression(_varName2).Publish += (_, i) =>
+            {
+                Var2 = i;
+            };
+        }
+
+        public event EventHandler<double>? Publish;
+    }
+    internal class MultiplyExpression : IExpression
+    {
+        private readonly string _varName1;
+        private readonly string _varName2;
+
+        private double? _var1;
+        public double? Var1
+        {
+            get => _var1;
+            set
+            {
+                if (_var1 == value)
+                {
+                    return;
+                }
+                _var1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double? _var2;
+        public double? Var2
+        {
+            get => _var2;
+            set
+            {
+                if (_var2 == value)
+                {
+                    return;
+                }
+                _var2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged()
+        {
+            if (Var1 != null && Var2 != null)
+            {
+                LastValue = Var1.Value * Var2.Value;
+                Publish?.Invoke(this, LastValue.Value);
+            }
+        }
+
+        public MultiplyExpression(string varName1, string varName2)
+        {
+            _varName1 = varName1;
+            _varName2 = varName2;
+        }
+
+        public double? LastValue { get; private set; }
+
+        public void Setup(ExecutionContext context)
+        {
+            context.GetExpression(_varName1).Publish += (_, i) =>
+            {
+                Var1 = i;
+            };
+            context.GetExpression(_varName2).Publish += (_, i) =>
+            {
+                Var2 = i;
+            };
+        }
+
+        public event EventHandler<double>? Publish;
+    }
+    internal class DivideExpression : IExpression
+    {
+        private readonly string _varName1;
+        private readonly string _varName2;
+
+        private double? _var1;
+        public double? Var1
+        {
+            get => _var1;
+            set
+            {
+                if (_var1 == value)
+                {
+                    return;
+                }
+                _var1 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double? _var2;
+        public double? Var2
+        {
+            get => _var2;
+            set
+            {
+                if (_var2 == value)
+                {
+                    return;
+                }
+                _var2 = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void OnPropertyChanged()
+        {
+            if (Var1 != null && Var2 != null)
+            {
+                LastValue = Var1.Value / Var2.Value;
+                Publish?.Invoke(this, LastValue.Value);
+            }
+        }
+
+        public DivideExpression(string varName1, string varName2)
+        {
+            _varName1 = varName1;
+            _varName2 = varName2;
+        }
+
+        public double? LastValue { get; private set; }
+
+        public void Setup(ExecutionContext context)
+        {
+            context.GetExpression(_varName1).Publish += (_, i) =>
+            {
+                Var1 = i;
+            };
+            context.GetExpression(_varName2).Publish += (_, i) =>
+            {
+                Var2 = i;
+            };
+        }
+
+        public event EventHandler<double>? Publish;
+    }
+    
+
+    internal class ExecutionContext
+    {
+        private readonly Dictionary<string, IExpression> _expressions = new();
+
+        public IExpression GetExpression(string name)
+        {
+            return _expressions[name];
+        }
+
+        public void AddExpression(string name, IExpression expression)
+        {
+            if (_expressions.ContainsKey(name))
+            {
+                _expressions.Remove(name);
+            }
+            _expressions.Add(name, expression);
+        }
+
+        public void Init()
+        {
+            foreach (var expression in _expressions.Select(x => x.Value))
+            {
+                expression.Setup(this);
+            }
+        }
+
+        public void RefreshAllNumberValues()
+        {
+            foreach (var expression in _expressions.Select(x => x.Value))
+            {
+                if (expression is NumberExpression numberExpression)
+                {
+                    numberExpression.PublishValue();
+                }
+            }
+        }
+    }
+
+    internal interface IExpression
+    {
+        public double? LastValue { get; }
+
+        void Setup(ExecutionContext context);
+
+        public event EventHandler<double> Publish;
+    }
+}
